da089fd98a523dd612bb23e9600105fc51d00c24
Yasuhito Nishide <yasuhitonishide@gmail.com>
2021-01-30 19:04:57 +0900

    edit_Nishide  commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zebra
+                    ZebraApp
                 </div>
 
                 <div class="links">
